d83287c4c771adeafbda417f4ac9a6890b5def7d
Endahrk <endahretno040@gmail.com>
2025-11-17 15:08:37 +0700

    perbaiki code

--- resources/views/bantuan.blade.php
+++ resources/views/bantuan.blade.php
@@ -6,13 +6,15 @@
             {{ session('success') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-    @endif
+    @endif 
+
     @if (session('error'))
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             {{ session('error') }}
             <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
         </div>
-    @endif
+    @endif 
+    
     @if ($errors->any())
         <div class="alert alert-danger alert-dismissible fade show" role="alert">
             <strong>Whoops! Terjadi kesalahan:</strong>
